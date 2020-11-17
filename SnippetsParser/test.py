1d929ccac902f5f645cf222422acf6253be26114
SammyTaleb <sammy.talebphotmail.fr>
2020-11-17 14:20:37 +0100

    Adding Query Function and csv export

--- SnippetsParser/test.py
+++ SnippetsParser/test.py
@@ -1,5 +1,4 @@
 import nest_asyncio
-import time
 from search_engine_parser.core.engines.google import Search as GoogleSearch
 from search_engine_parser.core.engines.bing import Search as BingSearch
 from search_engine_parser.core.engines.yahoo import Search as YahooSearch
@@ -7,24 +6,44 @@ from search_engine_parser.core.engines.github import Search as GithubSearch
 from search_engine_parser.core.engines.duckduckgo import Search as DuckDuckGoSearch
 import pandas as pd
 
-def snippetsParser(query,search_engine_nbr=0):
+
+
+def snippetsParser(person,_query,search_engine_nbr=0):
     gsearch=GoogleSearch()
     bsearch=BingSearch()
     ysearch=YahooSearch()
     gitsearch=GithubSearch()
     dsearch=DuckDuckGoSearch()
     search_engines=[gsearch,bsearch,ysearch,gitsearch,dsearch]
-    #nest_asyncio.apply() #allow the following event to be nested
-    search_args=(query,3)
+    query=person+' '+_query
+    nest_asyncio.apply() #allow the following event to be nested
+    search_args=(query,1)
     results=search_engines[search_engine_nbr].search(*search_args)
-    snippets_frame=pd.DataFrame([i for i in results] )
+    snippets_frame=[]
+    try:
+        for i in range(len(results)):
+            snippets_frame.append({'site':results[i]['links'],'engine_search':search_engine_nbr,
+                                   'id_person':person,'search':query,'title':results[i]['titles'],'text':results[i]['descriptions']})
+    except:
+        pass
     return snippets_frame
 
-deb=time.time()   
-results=snippetsParser('Google history',0)
-print(results)
-print(time.time()-deb)
+def query(personnes,_query,search_engine_nbr=0):
+    results=[]
+    for person in personnes:
+        res=snippetsParser(person,_query,search_engine_nbr)
+        for ele in res:
+            results.append(ele)
+    return pd.DataFrame(results)
 
+if __name__=='__main__':
+    personnes=['Sammy Taleb','Pegah Alizadeh','Tristan Darrigol','Donald Trump','Joe Biden','Barack Obama','Emmanuel Macron']
+    results=query(personnes,'university',0)
+    for i in range(len(results['site'])):
+        if int(results['engine_search'][i])==0:
+            results['site'][i]=results['site'][i][29:]
+    results.to_csv('../DiasporaEnv/DiasporaGym/data/big.csv')
+    print(results)
 
 
 
